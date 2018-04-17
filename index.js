556734eec0c746631c71ae759bfc420ef97894d5
carlostrapet <trapet_llamas@hotmail.com>
2018-04-17 17:59:42 +0100

    :building_construction: chore: added express routing for Photo and Album

--- index.js
+++ index.js
@@ -5,4 +5,20 @@ app.get('/', (req, res) => {
   res.end()  
 });
 
+var photoRouter = express.Router();
+photoRouter.get('/', (req, res) => {});
+photoRouter.post('/', (req, res) => {});
+photoRouter.get('/:id', (req, res) => {});
+photoRouter.patch('/:id', (req, res) => {});
+photoRouter.delete('/:id', (req, res) => {});
+app.use('/photo', photoRouter);
+
+var albumRouter = express.Router();
+albumRouter.get('/', (req, res) => { });
+albumRouter.post('/', (req, res) => { });
+albumRouter.get('/:id', (req, res) => { });
+albumRouter.patch('/:id', (req, res) => { });
+albumRouter.delete('/:id', (req, res) => { });
+app.use('/album', albumRouter);
+
 module.exports = app;
